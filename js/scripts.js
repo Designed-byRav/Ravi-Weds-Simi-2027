530e08f975a33b4d8b3f1cccd5ea2a69d7253239
Designed-byRav <ravi222bhatia@gmail.com>
2025-03-01 21:44:25 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -241,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 51.5014, lng: 0.1419};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
